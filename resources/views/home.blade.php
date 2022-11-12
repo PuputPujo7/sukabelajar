2fe5cd4a8da417b949bd46c8a06bc119b557f66c
PuputPujo7 <puputpujo7@gmail.com>
2022-11-12 20:14:50 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,306 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/dist/output.css" rel="stylesheet">
+</head>
+<body id="default_theme" class="home_page1" style="overflow: visible;"><div id="preloader" style="display: none;"><div class="spinner-sm spinner-sm-1" id="status" style="display: none;"> </div></div>
+    <div id="beranda"></div>
+    <!-- header -->
+    <header class="header header_style1" style="position: fixed;background-color: white;">
+       <div class="container">
+          <div class="row">
+             <div class="col-md-9 col-lg-12">
+                <div class="logo"><a href=""><img src="themes/auricle/images/logo.png" alt="#"></a></div>
+                <div class="main_menu float-right" style="margin-top:2px;">
+                   <div class="menu">
+                      <a href="#" class="menu-mobile">&nbsp;</a><ul class="clearfix">
+                         <li class="active"><a href="#beranda">Beranda</a></li>
+                         <li><a href="#ruangpeduli">Media Peduli</a></li>
+                         <li><a href="#tentangkami">Tentang Kami</a></li>
+<!--                           <li><a href="#ruangkonsultasi" title="Coming soon">Ruang Konsultasi</a></li>
+                         <li><a href="#ruangwirausaha" title="Coming soon">Ruang Wirausaha</a></li> -->
+                      </ul>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+    </header>
+    
+    <section id="banner_parallax" class="layout_padding gradiant_bg cross_layout" style="padding-top:50px; margin-top: 10px;">
+       <div class="container">
+          <div class="row">
+             <div class="col-md-12">
+                <div class="full">
+                   <div class="slide_cont">
+                      <h2><font color="#8B0000">KREATIF</font> <font color="#FF8C00">INOVATIF</font><br><span><font color="#008000">MANDIRI</font></span></h2>
+                      <p>BERSAMA MEMBANGUN POTENSI DIRI DENGAN CARA BERBAGI ILMU PENGETAHUAN<br>
+          Tumbuhkan Kecakapan Diri Yang Kreatif, Inovatif dan Mandiri</p>
+                      <div class="full slide_bt">
+                      <a class="white_bt bt_main" style="border-radius:10px;" href="https://mediabelajar.p4ib.com/home/?v=login">LOGIN</a>
+                      </div>
+                   </div>
+                </div>
+             </div>
+<!--               <div class="col-md-6">
+                <div class="full">
+                   <div class="slide_pc_img wow fadeInRight" data-wow-delay="1s" data-wow-duration="2s"> 
+                   <img src="themes/auricle/images/back-pc.png" alt="#" />
+                   </div>
+                </div>
+             </div>
+-->               
+          </div>
+       </div>
+    </section>
+    <!-- end header -->
+    <!-- section -->
+    <section class="layout_padding padding_top_0">
+       <div class="container">
+          <div class="row margin_bottom_30">
+             <!-- featured cont -->
+             <div class="col-md-6 cont_theme_blog">
+                <div class="full">
+                   <h3>GERAKAN LITERASI</h3>
+                   <h4>Dengan Membaca Dan Menulis Kita Tumbuhkan Budi Pekerti</h4>
+                   <p>Peran vital literasi untuk menghasilkan sumber daya manusia unggul dengan cara belajar sepanjang hayat, menguasai kompetensi global dan dicirikan oleh nilai-nilai Pancasila dengan kriteria beriman, bertakwa kepada Tuhan yang maha esa dan berakhlak; mandiri; bernalar kritis; kreatif: bergotong royong; dan berkebinekaan global.</p>
+                </div>
+             </div>
+             <!-- end featured cont -->
+             <!-- featured image -->
+             <div class="col-md-6 wow fadeInRight" data-wow-delay="0.5" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-name: fadeInRight;">
+                <div class="full">
+                   <div class="center"><img src="themes/auricle/images/poster_5.jpg" alt="#" width="100%" style="border-radius:10px;box-shadow: 0 0px 10px 0 rgba(0, 0, 0, 0.2), 0 0px 20px 0 rgba(0, 0, 0, 0.19);"></div>
+
+                </div>
+             </div>
+             <!-- end featured image -->
+          </div>
+          <div class="row">
+             <!-- featured cont -->
+             <div class="col-md-12 cont_theme_blog">
+                <div class="full">
+                   <center><h3>BERSAMA MEMBANGUN POTENSI DIRI</h3></center>
+                </div>
+             </div>
+             <!-- end featured cont -->
+          </div>
+        </div>
+    </section>
+    <!-- end section -->
+    <!-- section -->
+    <section class="layout_padding gradiant_bg cross_layout">
+       <div class="container">
+          <div class="row">
+             <div class="col-md-4">
+                <div class="information_blogs">
+                   <div class="imf_icon"><img src="themes/auricle/images/icon1.png" alt="#"></div>
+                   <div class="imf_head">
+                      <h3>Pengembangan Diri</h3>
+                   </div>
+                   <div class="imf_cont">
+                      <p>Menumbuhkan Kecakapan Diri Yang Kreatif, Inovatif dan Mandiri</p>
+                   </div>
+                </div>
+             </div>
+             <div class="col-md-4">
+                <div class="information_blogs">
+                   <div class="imf_icon"><img src="themes/auricle/images/icon2.png" alt="#"></div>
+                   <div class="imf_head">
+                      <h3>Kepercayaan</h3>
+                   </div>
+                   <div class="imf_cont">
+                      <p>Mengutamakan unsur kepercayaan dalam berkolaborasi</p>
+                   </div>
+                </div>
+             </div>
+             <div class="col-md-4">
+                <div class="information_blogs">
+                   <div class="imf_icon"><img src="themes/auricle/images/icon3.png" alt="#"></div>
+                   <div class="imf_head">
+                      <h3>Kerjasama</h3>
+                   </div>
+                   <div class="imf_cont">
+                      <p>Menciptakan kerjasama yang jujur, komitmen dan saling melengkapi</p>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+    <div id="ruangpeduli"></div>
+    </section>
+    <!-- end section -->
+    <!-- section -->
+    <section class="layout_padding layer_style">
+       <div class="container">
+          <div class="row">
+             <div class="col-sm-12">
+                <div class="full text_align_center">
+                   <div class="heading_main center_head_border heading_style_1">
+                      <h2>MEDIA <span>PEDULI</span></h2>
+                   </div>
+                </div>
+             </div>
+          </div>
+          <div class="row app-features">
+             <div class="col-sm-6">
+                  <center>
+                  <i><h5>"Kepedulian kita hari ini akan memberikan perbedaan berarti pada masa depan"<br>.:- Morrie Schwartz -:." </h5></i>
+                  <hr><p><strong>Salurkan Kepedulian anda melalui beberapa media berikut ini atau di area terdekat sekitar tempat tinggal kita masing-masing:</strong></p><br>
+<!--                     <img src="page/adv/img/peduli.jpg" width="60%" class="img-fluid" alt=""><br> -->
+<div class="wow fadeInRight" data-wow-delay="0.5" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-name: fadeInRight;">
+                  <a href="https://www.baznasjabar.org/infak" target="_blank"><img src="img-peduli/baznas.png" width="25%" class="img-fluid" alt=""></a>
+                  <a href="" target="_blank"><img src="https://e-bdg.com/donasiapp.jpg" width="25%" class="img-fluid" alt=""></a>
+                  <a href="https://donasi.dompetdhuafa.org/" target="_blank"><img src="img-peduli/ddhuafa.png" width="25%" class="img-fluid" alt=""></a>
+                  <a href="https://dtpeduli.org/donasi/" target="_blank"><img src="img-peduli/dtpeduli.png" width="25%" class="img-fluid" alt=""></a>
+                  <a href="https://www.yaimsa.or.id/kontak/" target="_blank"><img src="img-peduli/yaimsa.png" width="25%" class="img-fluid" alt=""></a>
+                  <a href="https://buletin.husnunnisa.my.id/" target="_blank"><img src="https://buletin.husnunnisa.my.id/wp-content/uploads/2022/09/colormag-logo.png" width="25%" class="img-fluid" alt=""></a>
+</div>
+                  <br>
+                   
+                  </center>
+             </div>
+             <div class="col-sm-6">
+                  <center><h3>Rajut Solidaritas dan Rasa Kepedulian</h3>
+                  <p><br>
+                  Wujud yang paling sederhana dari empati adalah bersedekah.
+                  Melalui media belajar online mari kita belajar bersama untuk membiasakan diri bersedekah dan menghilangkan rasa egoisme dan kebencian
+                  dengan cara menumbuhkan rasa empati terhadap orang lain.<br>
+                  </p>
+                  <p>
+                  <font size="3"><em>Mari kita sisihkan sebagian harta dan keuntungan kita untuk membantu orang lain</em></font>
+                  <br><br>
+                  <a href="https://www.supportunicefindonesia.org/?utm_source=Google&amp;utm_medium=SEM&amp;utm_content=Ads3&amp;utm_campaign=BackToSchoolJun2021&amp;utm_redirect=1" target="_blank" alt="RUANGbelajar: UNICEF Indonesia">
+                      <img src="img/unicef-indonesia.png" width="100%">
+                  </a>
+                  <!-- <hr>
+                  <a href="https://balsawoodindonesia.com/" target="_blank"><img src="https://balsawoodindonesia.com/img/logoweb.jpg"  width="25%" class="img-fluid" alt=""></a>
+                  <a href="https://sobat-edu.com/" target="_blank"><img src="https://sobat-edu.com/assets/img/about.jpg"  width="25%" class="img-fluid" alt=""></a> -->
+                  </p></center>
+             </div>
+          </div>
+       </div>
+    </section>
+    <!-- end section -->
+    <!-- section -->
+    <section class="layout_padding gradiant_bg cross_layout">
+       <div class="container">
+          <div class="row">
+             <div class="col-sm-12">
+                <div class="full text_align_center white_fonts">
+                   <div class="heading_main center_head_border heading_style_1">
+                      <h2>Ayo!, <span> Kita Dukung Gerakan Literasi Nasional...!</span></h2>
+                   </div>
+                </div>
+             </div>
+          </div>
+          <div class="row step_section">
+             <div class="offset-xl-1 col-xl-10 col-md-12">
+                <div class="row">
+                   <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                      <div class="step_blog arrow_right_step">
+                         <div class="step_inner">
+                            <i class="fa fa-building"></i><br>
+                            <p>Gerakan Literasi Sekolah</p>
+                         </div>
+                      </div>
+                   </div>
+                   <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                      <div class="step_blog">
+                         <div class="step_inner">
+                            <i class="fa fa-home"></i><br>
+                            <p>Gerakan Literasi Keluarga</p>
+                         </div>
+                      </div>
+                   </div>
+                   <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                      <div class="step_blog">
+                         <div class="step_inner">
+                            <i class="fa fa-users"></i><br>
+                            <p>Gerakan Literasi Masyarakat</p>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+    </section>
+    <!-- end section -->
+    <!-- footer -->
+    <footer class="footer_style_2" id="tentangkami">
+       <div class="footer_top">
+          <div class="container">
+             <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 margin_bottom_30">
+                   <div class="full width_9" style="margin-bottom:25px;"> <a href="index.html">
+                   <img class="img-responsive" width="250" src="themes/auricle/images/logo.png" alt="#"></a> </div>
+                   <div class="full width_9">
+                      <p>Komunitas Media Belajar Online dapat menjembatani Kegiatan Belajar Mengajar Jarak Jauh (Online) yang diperuntukkan bagi Institusi Pendidikan, Guru/Tutor dan Siswa/Peserta Didik</p>
+                   </div>
+                   <div class="full width_9">
+                        <a style="border-radius:10px;" class="bt_main" href="https://mediabelajar.p4ib.com/home/?reg=1">Daftar Sekarang</a>
+                   </div>
+                   <div class="full width_9"><br>
+                   <h5>
+                   <i class="fa fa-users"></i> 253 Pendidik<br>
+                   <i class="fa fa-users"></i> 1593 Peserta Didik
+                   </h5>
+                   </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 margin_bottom_30">
+                   <div class="full">
+                      <div class="footer_blog_2 width_9">
+                         <h3>Institusi Pendidikan</h3>
+                         <p>
+                         Institusi pendidikan adalah sebuah lembaga badan yang menyelenggarakan kegiatan pendidikan, belajar-mengajar, dan/atau pelatihan.
+                         </p>
+                      </div>
+                   </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-2 margin_bottom_30">
+                   <div class="full">
+                      <div class="footer_blog_2">
+                         <h3>Pendidik </h3>
+                         <p>
+                         Pendidik adalah seseorang atau kelompok yang melaksanakan proses pembelajaran dan bertindak sebagai salah satu motivator atau sumber belajar dari kegiatan pendidikan, belajar-mengajar, dan/atau pelatihan.
+                         </p>
+                      </div>
+                   </div>
+                   <div class="full">
+                   </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 margin_bottom_30">
+                   <div class="full">
+                      <div class="footer_blog_2 width_9">
+                         <h3>Peserta Didik </h3>
+                         <p>
+                         Peserta didik merupakan peserta yang mengikuti pembelajaran yang diselenggarakan di intitusi pendidikan, dengan tujuan untuk menjadi manusia yang berilmu pengetahuan, berketerampilan, berpengalaman, berkepribadian, berakhlak mulia dan mandiri.
+                         </p>
+                      </div>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </div>
+       <!-- footer bottom -->
+       <div class="footer_bottom">
+          <p>Powered by <strong>www.p4ib.com</strong> @2020</p>
+       </div>
+    </footer>
+    <!-- end footer -->
+    <!--=========== js section ===========-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript) -->
+    <script src="themes/auricle/js/jquery.min.js"></script>
+    <script src="themes/auricle/js/popper.min.js"></script>
+    <script src="themes/auricle/js/bootstrap.min.js"></script>
+    <!-- wow animation -->
+    <script src="themes/auricle/js/wow.js"></script>
+    <!-- custom js -->
+    <script src="themes/auricle/js/custom.js"></script>
+ 
+
+</body>
+</html>
